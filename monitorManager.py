3b415974953e646881fd9fb66a950acec8ca8b47
Zehks <alberto95gonzalez@gmail.com>
2023-02-13 14:26:06 +0100

    Fix

--- monitorManager.py
+++ monitorManager.py
@@ -4,19 +4,20 @@ class MonitorManager():
 
     def __init__(self):
         self.directories = []
-        self.total = 0
+        
     
     def get_fs_size(self, path: str) -> int:
+        total = 0
 
         with os.scandir(path) as it:
             for entry in it:
                 if entry.is_file():
-                    self.total += entry.stat().st_size
+                    total += entry.stat().st_size
                 elif entry.is_dir():
                     self.directories.append(entry.path)
-                    self.total += self.get_fs_size(entry.path)
+                    total += self.get_fs_size(entry.path)
 
-        return self.total
+        return total
 
     def reset_directories_listing(self):
         self.directories = [] 
